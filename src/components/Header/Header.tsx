a1743ec7762f21d7007b186e36dfa5f70f36286b
a.lihtgolt <a.lihtgolt@gmail.com>
2023-09-25 01:23:20 +0300

    initial repositories

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import { CartIcon } from "../CartIcon/CartIcon"
+import { Logo } from "../Logo/Logo"
+import { SearchIcon } from "../SearchIcon/SearchIcon"
+import { UserIcon } from "../UserIcon/UserIcon"
+
+export const Header = () => {
+    return <header className="w-full flex flex-col justify-center">
+        <div className="w-full flex flex-col items-center justify-center py-[30px] border-b">
+            <div className="container flex justify-between px-[15px]">
+                <div>US-EN</div>
+                <Logo />
+                <div className="flex gap-[30px]">
+                    <SearchIcon />
+                    <CartIcon />
+                    <UserIcon />
+                </div>
+            </div>
+        </div>
+        <nav>
+            <ul className="flex py-[10px] justify-center gap-[60px] text-[12px] uppercase font-[300] ">
+                <li>NEW</li>
+                <li>DENIM</li>
+                <li>Bottoms</li>
+                <li>Tops</li>
+                <li>Bags</li>
+                <li>Every day basics</li>
+                <li>Sale</li>
+                <li>Coming soon</li>
+            </ul>
+        </nav>
+    </header>
+}
---
+++ src/components/SearchIcon/SearchIcon.tsx
@@ -0,0 +1,6 @@
+export const SearchIcon = () => {
+    return <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19 19.9476L14.7 15.6476M17 9.94764C17 14.3659 13.4183 17.9476 9 17.9476C4.58172 17.9476 1 14.3659 1 9.94764C1 5.52936 4.58172 1.94764 9 1.94764C13.4183 1.94764 17 5.52936 17 9.94764Z" stroke="#1D1D1D" stroke-linecap="square" />
+    </svg>
+
+}
