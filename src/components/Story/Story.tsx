66297b192bfd3df9288066f5a7cc61a97532dd41
Vlsdos Medelian <vlad93gm@gmail.com>
2023-11-26 17:37:29 +0200

    img need

---
+++ src/components/Story/Story.tsx
@@ -0,0 +1,15 @@
+export const Story = () => {
+  return (
+    <div className="container mx-auto my-[30px] px-[15px] max-w-[768px] flex flex-col gap-[15px]">
+      <h1 className="text-center text-[32px] font-bold">НАША МИССИЯ</h1>
+      <p className="text-center text-[25px]">
+        Parallels - это не просто магазин одежды, это место для тех, кто ценит
+        стиль и инновации. Мы верим в то, что каждый человек уникален. Наша цель
+        - стать глобальной площадкой для моды, где встречаются творцы, кураторы
+        и ценители стиля.
+      </p>
+      <h2 className="text-center text-[25px] font-bold">О НАС</h2>
+      {/* Тут компонента фоток и описания */}
+    </div>
+  );
+};
